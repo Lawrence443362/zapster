be6fe017327bffc6434e2def41d07dae3aa94b5d
Lavrentiy <kanlll43362@gmail.com>
2025-06-13 14:13:05 +0500

    Created auth

---
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Resources\V1\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(StoreUserRequest $request)
+    {
+        $user = User::create($request->all());
+
+        return new UserResource($user);
+    }
+
+    public function login(LoginUserRequest $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                "message" => "Wrong email or password"
+            ], 401);
+        }
+
+        $user = Auth::user();
+
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json([
+            "user" => new UserResource($user),
+            "token" => $token
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\PostContoller;
 use App\Http\Controllers\Api\V1\TagController;
 use Illuminate\Http\Request;
@@ -9,9 +10,17 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::prefix('/v1')
+Route::prefix('/v1/auth')
     ->middleware('throttle:api')
     ->group(function () {
+        Route::post('register', [AuthController::class, 'register'])->name('user.register;');
+        Route::post('login', [AuthController::class, 'login'])->name('user.login');
+    });
+
+    Route::prefix('/v1')
+    ->middleware(['throttle:api', 'auth:sanctum'])
+    ->group(function () {
+        Route::get('auth/logout', [AuthController::class, 'logout'])->name('user.logout');
         Route::apiResource('/tags', TagController::class);
         Route::apiResource('/posts', PostContoller::class);
     });
