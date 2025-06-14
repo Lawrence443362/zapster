992e8d61ac1e013aa68a81b512f8baba4e054e2b
Lavrentiy <kanlll43362@gmail.com>
2025-06-14 13:03:11 +0500

    Added profile route

--- routes/api.php
+++ routes/api.php
@@ -4,24 +4,22 @@ use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\PostController;
 use App\Http\Controllers\Api\V1\TagController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::prefix('/v1/auth')
     ->middleware(['throttle:api'])
     ->group(function () {
-        Route::post('register', [AuthController::class, 'register'])->name('user.register;');
-        Route::post('login', [AuthController::class, 'login'])->name('user.login');
+        Route::post('/register', [AuthController::class, 'register'])->name('user.register;');
+        Route::post('/login', [AuthController::class, 'login'])->name('user.login');
     });
 
 Route::prefix('/v1')
     ->middleware(['throttle:api', 'auth:sanctum'])
     ->group(function () {
-        Route::get('auth/logout', [AuthController::class, 'logout'])->name('user.logout');
-        Route::apiResource('/tags', TagController::class);
-        Route::apiResource('/posts', PostController::class);
+        Route::get('/auth/logout', [AuthController::class, 'logout'])->name('user.logout');
+        Route::get('/auth/profile', fn(Request $request) => Auth::user());
+        Route::apiResource('tags', TagController::class);
+        Route::apiResource('posts', PostController::class);
     });
 
