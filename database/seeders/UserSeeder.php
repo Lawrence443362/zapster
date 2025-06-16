2ebbf9cda7b6a502f2562cbef6a41f9f5134471e
Lavrentiy <kanlll443362@gmail.com>
2025-06-16 11:20:20 +0500

    Fix seed

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,9 +12,12 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::firstOrCreate(
+            ['email' => 'test@example.com'],
+            User::factory()->make([
+                'name' => 'Test User',
+                'email' => 'test@example.com',
+            ])->toArray()
+        );
     }
 }
